918abb66899580d99bdf1123766c63e720ae072e
Michael Zheng <michaelzheng04@gmail.com>
2021-07-23 18:48:15 -0400

    Detailed errors

--- discord-player/error.js
+++ discord-player/error.js
@@ -1,5 +1,27 @@
 module.exports = (client, error, message) => {
-    message.channel.send(`An error has occured! Please contact the developer \n Error: ${error}`).then(function(message) {
-        message.delete({timeout: 120000});
-    })
+    const sendMessage = client.functions.get('sendMessageTemp')
+    if (error = "NotPlaying") {
+        sendMessage.execute(message, "The bot is currently not playing anything!")
+        console.error("NotPlaying Error")
+    } else if (error = "UnableToJoin") {
+        sendMessage.execute(message, "Could not join the channel!")
+        console.error("UnableToJoin Error")
+    } else if (error = "NotConnected") {
+        sendMessage.execute(message, "The bot is currently not connected!")
+        console.error("NotConnected Error")
+    } else if (error = "ParseError") {
+        sendMessage.execute(message, "The bot could not parse. \n Please try again!")
+        console.error("ParseError Error")
+    } else if (error = "LiveVideo") {
+        sendMessage.execute(message, "Something live video error")
+        console.error("LiveVideo Error")
+    } else if (error = "VideoUnavailable") {
+        sendMessage.execute(message, "This video is currently unavailable!")
+        console.error("VideoUnavailable Error")
+    } else {
+        message.channel.send(`An error has occured! Please contact the developer \n Error: ${error}`).then(function(message) {
+            message.delete({timeout: 120000});
+        })
+        console.error(`Unknown error \n Erorr: ${error}`)
+    }
 }
