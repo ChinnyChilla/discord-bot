c18d6d21b86202cbe2a980cecb1147867840cd56
ChinnyChilla <michaelzheng04@gmail.com>
2021-08-02 18:46:36 -0400

    send message when skipping

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -4,13 +4,17 @@ module.exports = {
     description: 'Skips song(s)',
     args: '[Amount of times]',
     execute(client, message, args) {
+        const sendMessage = client.functions.get('sendMessageTemp')
         if (!args[0]) {
             client.player.skip(message)
+            sendMessage.execute(message, "Skipping!")
         }
         else if (args[0].match(/^[0-9]+$/)) {
              client.player.jump(message, parseInt(args[0]))
+             sendMessage.execute(message, `Skipping ${args[0]} times!`)
         } else {
             client.player.skip(message)
+            sendMessage.execute(message, "Skipping!")
         }
     }
 }
