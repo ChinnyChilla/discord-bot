dc23ed8e4e86bd6027b3e24f40ceff1850423b80
ChinnyChilla <michaelzheng04@gmail.com>
2021-08-02 20:20:56 -0400

    added reaction to message

--- discord-player/queueCreate.js
+++ discord-player/queueCreate.js
@@ -1,6 +1,12 @@
 module.exports = (client, message, queue) => {
-    message.channel.send('\u200B').then(function (message) {
+    message.channel.send('\u200B').then(async function (message) {
         client.queueMessages.set(message.guild.id, message)
         console.log(`Set message to ${message.guild.id}`)
+        await message.react('❤️')
+        const filter = (reaction, user) => {return reaction.emoji.name === '❤️'}
+        const collector = message.createReactionCollector(filter)
+        collector.on('collect', (reaction, user) => {
+            console.log("Collected one react")
+        })
     })
 }
