a53e68610112bfe1a57d46ca11fc198bea076f4e
ChinnyChilla <michaelzheng04@gmail.com>
2021-08-16 01:13:36 -0400

    updated to use interactions and discordjs v13

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
-const Discord = require('discord.js');
-const client = new Discord.Client()
-const config = require('./config.json');
+const { REST } = require('@discordjs/rest')
+const { Routes } = require('discord-api-types/v9')
+
+const { Client, Intents } = require('discord.js');
+
+const rest = new REST({version: '9'}).setToken(process.env.DISCORD_TOKEN)
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES]})
+// const config = require('./config.json');
 const fs = require('fs');
 
 const { Player } = require('discord-player')
 
-client.player = new Player(client, {leaveOnEnd: true, leaveOnEmpty: true,
-                                    leaveOnEmptyCooldown: 10000, autoSelfDeaf: true,
-                                    ytdlDownloadOptions: {filter: 'audioonly'}});
-client.config = config;
-client.commands = new Discord.Collection();
-client.queueMessages = new Discord.Collection();
-client.queueEmbeds = new Discord.Collection();
-client.queueIntervals = new Discord.Collection();
-client.functions = new Discord.Collection();
-client.queueReactionsCollections = new Discord.Collection();
+var commands = []
+client.player = new Player(client);
+// client.config = config;
+// client.commands = new Discord.Collection();
+client.queueMessages = new Array();
+client.queueEmbeds = new Array();
+client.queueIntervals = new Array();
+client.functions = new Array();
+client.queueReactionsCollections = new Array();
 
 console.log("Loading Events")
 
@@ -23,7 +27,12 @@ fs.readdirSync('./discordjs-events').forEach(file => {
         console.log("Loading discord.js file: " + file)
         const name = file.substring(0, file.length - 3)
         const event = require(`./discordjs-events/${file}`)
+        if (name == 'ready') {
+            client.once(name, event.bind(null, client))
+        } else {
         client.on(name, event.bind(null, client));
+        }
+        
     }
 })
 
@@ -46,20 +55,34 @@ fs.readdirSync('./commands').forEach(dir => {
         if (file.endsWith(".js")) {
             console.log("Loading command: " + file)
             var command = require(`./commands/${dir}/${file}`)
-            client.commands.set(command.name.toLowerCase(), command)
-        }
-    })
-})
-console.log("Comamnds Loaded!")
+            commands.push(command)
+            // client.commands.set(command.name.toLowerCase(), command)
+        };
+    });
+});
 
-console.log("Loading Functions")
+console.log("Comamnds Loaded!");
+async function setCommands() {
+    try {
+        console.log("Started refreshing application (/) commands.");
+
+        await rest.put(Routes.applicationCommands(process.env.DISCORD_CLIENT_ID), 
+        {body: commands});
+        console.log("Successfully reloaded application (/) commands.");
+    } catch (err) {
+        console.error(err);
+    };
+};
+setCommands()
+
+console.log("Loading Functions");
 fs.readdirSync('./functions').forEach(file => {
     if (file.endsWith('.js')) {
-        console.log("Loading function: " + file)
-        var func = require(`./functions/${file}`)
-        client.functions.set(file.substring(0, file.length - 3), func)
-    }
-})
+        console.log("Loading function: " + file);
+        var func = require(`./functions/${file}`);
+        client.functions.set(file.substring(0, file.length - 3), func);
+    };
+});
 console.log("Functions Loaded!")
 
-client.login(config.token)
+client.login(process.env.DISCORD_TOKEN)
