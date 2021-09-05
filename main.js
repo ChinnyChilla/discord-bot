ae09253b564cb8dd9c893bc0b9322e7aa3a28522
ChinnyChilla <63017068+ChinnyChilla@users.noreply.github.com>
2021-09-05 12:47:43 -0400

    added dotenv

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client()
 const config = require('./config.json');
 const fs = require('fs');
-
+require('dotenv').config()
 const { Player } = require('discord-player')
 
 client.player = new Player(client, {leaveOnEnd: true, leaveOnEmpty: true,
