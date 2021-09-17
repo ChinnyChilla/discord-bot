76acbf4f6e656dd616c9e68eb835b28f51bbd157
ChinnyChilla <michaelzheng04@gmail.com>
2021-09-17 18:07:34 -0400

    Fixed nonguild error

--- discordjs-events/interactionCreate.js
+++ discordjs-events/interactionCreate.js
@@ -4,7 +4,9 @@ module.exports = async (client, interaction) => {
     if (command.category == 'music' && !interaction.inGuild()) {
         interaction.reply("Music commands are only available in a guild!")
         setTimeout(() => {interaction.deleteReply()}, 30000)
-    }interaction.reply(`Recieved your ${command.name} command... please wait`).then(() => {
+        return
+    }
+    interaction.reply(`Recieved your ${command.name} command... please wait`).then(() => {
         setTimeout(() => interaction.deleteReply(), 30000)
         command.execute(client, interaction)
     })
