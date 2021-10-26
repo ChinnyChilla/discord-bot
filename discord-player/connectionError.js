a019efbabe2ea4074748c6b8c913b12f8409a624
ChinnyChilla <michaelzheng04@gmail.com>
2021-10-26 14:38:21 -0400

    rewording

--- discord-player/connectionError.js
+++ discord-player/connectionError.js
@@ -1,6 +1,5 @@
 module.exports = (client, queue, error) => {
-    client.functions.get('log').execute(queue.guild.id, "Connection Error")
-    client.functions.get('log').execute(queue.guild.id, error)
+    client.functions.get('log').execute(queue.guild.id, `Connection Error: ${error}`)
     queue.metadata.channel.send("Connection Error, retrying").then(message => {
         setTimeout(() => {message.delete()}, 15000)
         queue.play(queue.tracks[0], {immediate: true})
