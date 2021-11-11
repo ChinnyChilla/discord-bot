f416ba7cf424ba3dbb95944126a8e1f94b7662bb
Michael <michelzheng04@gmail.com>
2021-11-11 16:05:02 -0500

    added clear command

--- commands/core/clear.js
+++ commands/core/clear.js
@@ -1,3 +1,4 @@
+const { MessageActionRow, Permissions, Message, MessageButton } = require('discord.js')
 module.exports = {
     name: 'clear',
     category: 'core',
@@ -9,7 +10,7 @@ module.exports = {
             name: "channel",
             description: "Channel to delete messages in",
             required: true,
-            channelType: ["GUILD_TEXT"]
+            channelType: [0]
         },
         {
             type: 4,
@@ -19,6 +20,54 @@ module.exports = {
         }
     ],
     execute(client, interaction) {
-        console.log("Got one")
+        if (!interaction.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) {
+            return interaction.editReply("You do not have the required permissions to execute this!")
+        }
+        const channel = interaction.options.getChannel('channel')
+        if (!channel.isText()) {return interaction.editReply("Can only delete messages in text channels!")}
+        const amount = interaction.options.getInteger('amount')
+        if (amount < 1 || amount > 100) {return interaction.editReply("Can only delete amounts between 1-99!")}
+        const row = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setCustomId('yes')
+                    .setStyle('DANGER')
+                    .setLabel("Do it!")
+            )
+            .addComponents(
+                new MessageButton()
+                .setCustomId('no')
+                .setStyle("SECONDARY")
+                .setLabel("STOP!")
+            )
+        interaction.editReply({
+            content: `You are about to delete **${amount}** messages from <#${channel.id}>.\n**Are you sure?**`,
+            components: [row]
+        })
+        const filter = (buttonInteraction) => {
+            return interaction.member.id === buttonInteraction.member.id
+        }
+
+        const collector = interaction.channel.createMessageComponentCollector({filter, max: 1, time: 15000})
+
+        collector.on('end', (collection) => {
+            if ((collection.first()?.customId) === 'yes') {
+                channel.bulkDelete(amount, true).then(messages => {
+                    client.functions.get('log').execute(interaction.guild.id, `Cleared ${amount} messages from ${channel.name}`)
+                    channel.send(`Successfully deleted ${messages.size} messages`).then(message => {
+                        setTimeout(() => {message.delete().catch(err => {
+                            if (err.httpStatus == 404) {
+                                console.log("Message already deleted")
+                            }})
+                        }, 8000)
+                    })
+                })
+            } else {
+                interaction.editReply({
+                    content: "Stopped!",
+                    components: []
+                })
+            }
+        })
     }
 }
