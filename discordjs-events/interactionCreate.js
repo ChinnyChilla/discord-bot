b8474eb60c61b19b8c38cbc5702f6cc03023f230
ChinnyChilla <michaelzheng04@gmail.com>
2021-11-10 19:24:40 -0500

    catch already deleted reply

--- discordjs-events/interactionCreate.js
+++ discordjs-events/interactionCreate.js
@@ -3,11 +3,19 @@ module.exports = async (client, interaction) => {
     const command = client.commands.find(command => command.name == interaction.commandName)
     if (command.category == 'music' && !interaction.inGuild()) {
         interaction.reply("Music commands are only available in a guild!")
-        setTimeout(() => {interaction.deleteReply()}, 30000)
+        setTimeout(() => interaction.deleteReply().catch(err => {
+            if (err.httpStatus == 404) {
+                console.log("Reply already deleted")
+            }
+        }), 30000)
         return
     }
     await interaction.deferReply()
-    setTimeout(() => interaction.deleteReply(), 30000)
+    setTimeout(() => interaction.deleteReply().catch(err => {
+        if (err.httpStatus == 404) {
+            console.log("Reply already deleted")
+        }
+    }), 30000)
     client.functions.get('log').execute(interaction.guildId, `${interaction.member.user.tag} requested command ${command.name}`)
     command.execute(client, interaction)  
 }
