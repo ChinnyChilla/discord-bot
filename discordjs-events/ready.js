cdcbd2067c09141e87c091836d20386f7db6cca0
ChinnyChilla <michaelzheng04@gmail.com>
2021-11-17 18:46:30 -0500

    added output of amount of connections

--- discordjs-events/ready.js
+++ discordjs-events/ready.js
@@ -1,6 +1,13 @@
+const date = require('date-and-time')
 module.exports = (client) => {
     console.log(`Bot is ready as ${client.user.tag}!`)
     console.log(`Currently in ${client.guilds.cache.size} server`)
     client.user.setPresence({ activities: [{ name: 'Type / to begin!', 
     type: 'STREAMING' }], status: 'online' })
+    setInterval(() => {
+        const now = new Date();
+        console.log(`[${date.format(now, 'HH:mm')}] Status Report:`)
+        console.log(`Current # of Connections: ${client.voice.adapters.size}`)
+    }, 1000 * 600)
+
 }
