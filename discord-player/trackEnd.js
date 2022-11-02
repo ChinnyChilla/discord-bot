0bc9ed5fbeecbcbb3fb1ff5efbd6db6ef585f683
ChinnyChilla <michaelzheng04@gmail.com>
2022-11-02 10:34:58 -0400

    updated discord-player, fixed guildCreate

--- discord-player/trackEnd.js
+++ discord-player/trackEnd.js
@@ -2,6 +2,7 @@ module.exports = (client, queue, track) => {
     client.functions.get('log').execute(queue.guild.id, `Track ${track.title} Ended`)
     const guildID = queue.guild.id
 	const queueInfo = client.queueInfo.get(queue.guild.id)
+	if (queueInfo == null) {return};
     queueInfo.clearQueueInterval()
     queueInfo.setInterval("")
 }
--- discordjs-events/guildCreate.js
+++ discordjs-events/guildCreate.js
@@ -1,17 +1,18 @@
 const fs = require('fs')
 const path = require('path')
 module.exports = (client, guild) => {
-        guild.channels.create('chinny-music-bot', {reason: "Create a music channel"}).then(channel => {
-        const reqPath = path.join(__dirname, '../data/serverConfig.json')
-        const serverConfig = require(reqPath)
-        serverConfig[guild.id] = client.defaultServerConfig
-        serverConfig[guild.id]['musicChannel'] = channel.id
-        client.musicChannels.push(channel.id)
-        client.musicChannelServers.push(guild.id)
-        fs.writeFile(reqPath, JSON.stringify(serverConfig), function(err) {
-            if (err) {
-                console.error(`An Error has occured! ${err}`)
-            }
-        })
-    })    
+	guild.channels.create({name: 'chinny-music-bot', reason: "Create a music channel"}).then(channel => {
+		const reqPath = path.join(__dirname, '../data/serverConfig.json')
+		const serverConfig = require(reqPath)
+		serverConfig[guild.id] = client.defaultServerConfig
+		serverConfig[guild.id]['musicChannel'] = channel.id
+		client.musicChannels.push(channel.id)
+		client.musicChannelServers.push(guild.id)
+		fs.writeFile(reqPath, JSON.stringify(serverConfig), function(err) {
+			if (err) {
+				console.error(`An Error has occured! ${err}`)
+			}
+		})
+		channel.send({content:"A help message"})
+	})    
 } 
