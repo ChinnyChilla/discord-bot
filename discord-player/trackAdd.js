60f270e4cf48b994cfe764c015b4167b367dbfb4
ChinnyChilla <michaelzheng04@gmail.com>
2023-03-15 11:52:50 -0400

    fix no queue error

--- discord-player/trackAdd.js
+++ discord-player/trackAdd.js
@@ -1,4 +1,4 @@
 module.exports = (client, queue, track) => {
-    client.functions.get('updateQueue').execute(client, queue)
     client.functions.get('log').execute(queue.guild.id, `Track ${track.title} added by ${track.requestedBy.tag}`)
+	client.functions.get('updateQueue').execute(client, queue)
 }
