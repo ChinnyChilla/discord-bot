ad1e0d3a0e843155edeb2604e232404a1af1bfe0
ChinnyChilla <michaelzheng04@gmail.com>
2024-06-21 23:15:50 -0400

    added error event logging

--- events/musicEvents.js
+++ events/musicEvents.js
@@ -19,3 +19,7 @@ player.events.on('disconnect', async (queue) => {
 player.events.on('emptyChannel', async (queue) => {
 	queueUtil.deleteQueue(queue);
 })
+player.events.on('error'), async (queue, error) => {
+	console.error("Player encountered an error");
+	console.error(error);
+}
